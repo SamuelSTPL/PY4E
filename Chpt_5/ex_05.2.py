2e000536d3eb97db69dc84267e76e0103a8ade10
Samuel St-Pierre Lariviere <samuelstpierrelariviere@gmail.com>
2020-08-06 13:20:02 -0400

    Add chapter 5

---
+++ Chpt_5/ex_05.2.py
@@ -0,0 +1,44 @@
+#Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
+#Once 'done' is entered, print out the largest and smallest of the numbers. 
+#If the user enters anything other than a valid number catch it with a try/except 
+#and put out an appropriate message and ignore the number. 
+#Enter 7, 2, bob, 10, and 4 and match the output below.
+
+#Initiate variables 
+smallest = None
+largest = None
+
+#Looping until false
+while True:
+
+    #Ask for user input
+    num = input('Enter number: ')
+
+    #Condition to break the loop
+    if num == 'done':
+        break
+
+    #Catching Not A Number errors
+    try:
+        number = int(num)
+
+    #Print the error message then skip the loop
+    except:
+        print('Must enter a valid number')
+        continue
+    
+    #Compare and set value to variables
+    if largest is None:
+        largest = number
+    elif number > largest:
+        largest = number
+
+    if smallest is None:
+        smallest = number
+    elif smallest > number:
+        smallest = number
+
+print('Maximum is :', largest)
+print('Minimum is :', smallest)
+
+
